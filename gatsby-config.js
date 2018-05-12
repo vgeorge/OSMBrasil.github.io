1056de64ad70c62e962bc0a111aa78633e3b66be
Vitor George <vitor.george@aparabolica.com.br>
2018-05-12 12:35:12 -0300

    Move site metadata to Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: 'OpenStreetMap Brasil',
+    description: "Informações sobre como mapear e participar da comunidade do OpenStreetMap.",
+    url: "http://www.openstreetmap.com.br",
+    twitter_username: "OSMBrasil",
+    github_username:  "OSMBrasil",
   },
   plugins: [
     'gatsby-plugin-react-helmet',
