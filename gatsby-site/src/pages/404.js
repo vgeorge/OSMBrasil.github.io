1fd2524f05ce245347c4885bec7cca1b242523c3
Vitor George <vitor.george@aparabolica.com.br>
2018-05-12 10:28:11 -0300

    Init migration to jamstack by bootstraping Gatsby site

---
+++ gatsby-site/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NotFoundPage = () => (
+  <div>
+    <h1>Não encontrada</h1>
+    <p>O endereço procurado não existe.</p>
+  </div>
+)
+
+export default NotFoundPage
---
+++ gatsby-site/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Reformulação do site</h1>
+    <p>A versão atual do site do OpenStreetMap Brasil é feita em jekyll e hospedada via Github Pages. Este é o primeiro esboço do site no processo de migração para uma <a href="https://jamstack.org">jamstack</a>.</p>
+  </div>
+)
+
+export default IndexPage
